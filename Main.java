ab69a18d23f676abc331d43a0d86829fefc1bdce
JP-2112 <jpcorenaa@eafit.edu.co>
2024-05-06 19:54:02 -0500

    Practica 3 Lenguajes JP

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+public static void MiNombre(){
+    System.out.println("Practica 3 Juan Pablo Corena");
+}
+
+    public static void main(String[] args) {
+ MiNombre();
+    }
+}
